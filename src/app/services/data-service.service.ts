5c0dd1085d15c08c3671ff323c5780041c8523a8
Johnson Osei Yeboah <johnyoat@gmail.com>
2022-11-18 21:59:31 -0600

    updated data service base url

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -8,7 +8,7 @@ import {EventModel} from "../models/event.model";
 })
 export class DataServiceService {
 
-  BASE_URL = "http://35.239.194.89:8081"
+  BASE_URL = "http://34.27.210.184:8081"
 
   constructor(private http:HttpClient) {
 
